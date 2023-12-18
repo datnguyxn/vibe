c83517f19a64020829a6435ae4a6f211ee6b8a42
Lam Nhu Trinh <lamnhutrinh@gmail.com>
2023-12-18 22:15:30 +0700

    implement aritist page and song

---
+++ app/src/main/java/com/vibe/vibe/fragments/PlaylistFragment.java
@@ -0,0 +1,85 @@
+package com.vibe.vibe.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.vibe.vibe.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link PlaylistFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class PlaylistFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private static final String TAG = SearchFragment.class.getSimpleName();
+    private ImageView blur_image, playlist_image;
+    private TextView playlist_name, playlist_size;
+    private RecyclerView rvplaylist_songs;
+
+    public PlaylistFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment PlaylistFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static PlaylistFragment newInstance(String param1, String param2) {
+        PlaylistFragment fragment = new PlaylistFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_playlist, container, false);
+        init(view);
+
+        return view;
+    }
+
+    private void init(View view) {
+        blur_image = view.findViewById(R.id.blur_image);
+        playlist_image = view.findViewById(R.id.playlist_image);
+        playlist_name = view.findViewById(R.id.playlist_name);
+        playlist_size = view.findViewById(R.id.playlist_size);
+        rvplaylist_songs = view.findViewById(R.id.rvplaylist_songs);
+    }
+}
